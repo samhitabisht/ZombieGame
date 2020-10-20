5a0491cc7fc9a2e32abb95489c09308c33425565
samhitabisht <68056370+samhitabisht@users.noreply.github.com>
2020-10-20 20:00:43 +0530

    Add files via upload

--- Sketch.js
+++ Sketch.js
@@ -1,21 +1,39 @@
-var player, playerImg, zombie, zombieImg;
+var player, playerImg, zombieImg1,zombieImg2, zombieGroup;
 
 function preload(){
 playerImg= loadImage("Images/NotShooting.png");
-zombieImg=loadImage("Images/Zombie.jpg");
+zombieImg1=loadImage("Images/zombieLeft.png");
+zombieImg2= loadImage("Images/zombieRight.png")
+
 }
 
 function setup(){
 createCanvas(displayWidth,displayHeight); 
+edges= createEdgeSprites();
 player=createSprite(850,850,40,40);
 player.addImage(playerImg);
 player.scale=2;
-zombie=createSprite(850,150,40,40)
-zombie.addImage(zombieImg);
-zombie.scale=2
+zombieGroup= new Group(); 
 }
 
 function draw(){
 background("white");
+spawnZombies();
+zombieGroup.bounceOff(edges);
 drawSprites();
+}
+
+function spawnZombies (){
+    if (frameCount % 100 ===0){
+        var zombie= createSprite(random(100,100), 150, 40, 40);
+        zombie.setVelocity(random(-4,4),random(0,5))
+        if (zombie.velocityX<0){
+            zombie.addImage(zombieImg1);
+        }
+        else {
+            zombie.addImage(zombieImg2);
+        }
+        zombieGroup.add(zombie);
+
+    }
 }
